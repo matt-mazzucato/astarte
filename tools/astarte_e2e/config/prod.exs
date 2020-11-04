8baf4fb1234a7050321bab94a8a12602486581e0
Mattia Mazzucato <matt.mazzucato@gmail.com>
2020-11-04 12:07:53 +0100

    Improve logs
    + debug messages are ignored in prod env
    + decouple deliver/1 from specific log messages
    
    Signed-off-by: Mattia Mazzucato <matt.mazzucato@gmail.com>

--- tools/astarte_e2e/config/prod.exs
+++ tools/astarte_e2e/config/prod.exs
@@ -18,7 +18,11 @@
 
 use Mix.Config
 
+config: :logger
+  compile_time_purge_matching: [
+    [level_lower_than: :info]
+  ],
+
 config :logger, :console,
-  level: :info,
   format: {PrettyLog.LogfmtFormatter, :format},
   metadata: [:module, :function, :tag]
